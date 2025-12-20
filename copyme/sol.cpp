4f1211308518b793e7552a83abc8c939f85ec2c6
Abraham <ae248abraham@gmail.com>
2025-12-20 19:05:55 +1100

    Simplifies placeholder io basename

--- copyme/sol.cpp
+++ copyme/sol.cpp
@@ -4,7 +4,7 @@ using namespace std;
 static inline void redirect_io(string basename);
 
 int main(void) {
-    redirect_io("TODO");
+    redirect_io(".");
 
     
 }
@@ -13,7 +13,7 @@ static string suffix_in = ".in";
 static string suffix_out = ".out";
 
 static inline void redirect_io(string basename) {
-    if (!basename.compare("TODO")) return;
+    if (!basename.compare(".")) return;
     freopen((basename + suffix_in).c_str(), "r", stdin);
     freopen((basename + suffix_out).c_str(), "w", stdout);
 }
