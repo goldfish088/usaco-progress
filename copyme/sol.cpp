66397f67c69838aebc64c7f308cdf13a0b10eca4
Abraham <ae248abraham@gmail.com>
2025-12-20 18:29:44 +1100

    Fixes incorrect inverted usage of std::string::compare

--- copyme/sol.cpp
+++ copyme/sol.cpp
@@ -13,6 +13,7 @@ static string suffix_in = ".in";
 static string suffix_out = ".out";
 
 static inline void redirect_io(string basename) {
+    if (!basename.compare("TODO")) return;
     freopen((basename + suffix_in).c_str(), "r", stdin);
     freopen((basename + suffix_out).c_str(), "w", stdout);
 }
