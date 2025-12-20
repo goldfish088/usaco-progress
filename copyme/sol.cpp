0491766cfd41df65318605212964314109421667
Abraham <ae248abraham@gmail.com>
2025-12-20 18:01:17 +1100

    Updates the copyme template
    Moved the redirect_io function below main so that the actual code
    is at the top and not below boilerplate.

--- copyme/sol.cpp
+++ copyme/sol.cpp
@@ -1,14 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+static inline void redirect_io(string basename);
+
+int main(void) {
+    redirect_io("TODO");
+
+    
+}
+
 static string suffix_in = ".in";
 static string suffix_out = ".out";
 
 static inline void redirect_io(string basename) {
     freopen((basename + suffix_in).c_str(), "r", stdin);
     freopen((basename + suffix_out).c_str(), "w", stdout);
-}
-
-int main(void) {
-    redirect_io("TODO");
 }
