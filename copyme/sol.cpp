3d207d4b653c6e811459ece23076e6b562000bc4
Abraham <ae248abraham@gmail.com>
2025-12-20 16:16:04 +1100

    Update base solve script template to be more aligned with USACO submission

--- copyme/sol.cpp
+++ copyme/sol.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+static string suffix_in = ".in";
+static string suffix_out = ".out";
+
+static inline void redirect_io(string basename) {
+    freopen((basename + suffix_in).c_str(), "r", stdin);
+    freopen((basename + suffix_out).c_str(), "w", stdout);
+}
+
 int main(void) {
+    redirect_io("TODO");
 }
